f621bf9d096d370a3d047543f27934fd00761aed
Katia Aresti <karesti@redhat.com>
2023-01-16 18:21:38 +0100

    Infinispan client enhancements * OpenTelemetry property for context propagation * Dev services for cross site * Dev services with tracing * Dev services doc in it's own file

--- extensions/infinispan-client/deployment/src/main/java/io/quarkus/infinispan/client/deployment/devservices/InfinispanDevServicesConfig.java
+++ extensions/infinispan-client/deployment/src/main/java/io/quarkus/infinispan/client/deployment/devservices/InfinispanDevServicesConfig.java
@@ -74,6 +74,37 @@ public class InfinispanDevServicesConfig {
     @ConfigItem
     public Optional<List<String>> artifacts;
 
+    /**
+     * When creating a Infinispan container with cross site replication
+     */
+    @ConfigItem
+    public Optional<String> site;
+
+    /**
+     * If you are running an Infinispan Server already in docker, if the containers use the same mcastPort they will form a
+     * cluster.
+     * Set a different mcastPort to create a separate cluster in Docker (e. 46656).
+     *
+     * A common use case in a local Docker development mode, is the need of having two different Infinispan Clusters
+     * with Cross Site Replication enabled.
+     * see
+     * https://github.com/infinispan/infinispan-simple-tutorials/blob/main/infinispan-remote/cross-site-replication/docker-compose/
+     */
+    @ConfigItem
+    public OptionalInt mcastPort;
+
+    /**
+     * Runs the Infinispan Server container with tracing enabled. Traces are disabled by default
+     */
+    @ConfigItem(name = "tracing.enabled", defaultValue = "false")
+    public Optional<Boolean> tracing;
+
+    /**
+     * Sets Infinispan Server otlp endpoint. Default value is http://localhost:4317
+     */
+    @ConfigItem(name = "tracing.exporter.otlp.endpoint", defaultValue = "http://localhost:4317")
+    public Optional<String> exporterOtlpEndpoint;
+
     @Override
     public boolean equals(Object o) {
         if (this == o)
